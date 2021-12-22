e3ade58c71f34226aa0e0af909381c0b36dce8a3
Danilo Araujo <dnlysid@gmail.com>
2021-12-22 10:04:05 -0300

    aprendendo expressões regulares (regex)

---
+++ lib-markdown/regex.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+function extraiLinks(texto) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const arrayResultados = [];
+    let temp;
+    while ((temp = regex.exec(texto)) !== null) {
+        arrayResultados.push({ [temp[1]]: temp[2] })
+    }
+    return arrayResultados;
+}
+
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Oops!'));
+}
+
+async function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    try {
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(extraiLinks(texto));
+    } catch (erro) {
+        trataErro(erro);
+    }
+}
+
+pegaArquivo('./arquivos/texto1.md');
