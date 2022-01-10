ab55e8a23fc8ce345566902be439efa2dc47a444
Danilo Araujo <dnlysid@gmail.com>
2022-01-10 18:18:26 -0300

    validando links

--- lib-markdown/cli.js
+++ lib-markdown/cli.js
@@ -7,13 +7,13 @@ const caminho = process.argv;
 async function processaTexto(caminhoDoArquivo) {
     const resultado = await pegaArquivo(caminhoDoArquivo[2]);
     if (caminho[3] === 'validar') {
-        console.log(chalk.yellow('Links Validados'), validaURLs(resultado));
+        console.log(chalk.yellow('Links Validados'), await validaURLs(resultado));
     } else {
         console.log(chalk.yellow('Lista de Links'), resultado)
     }
 }
 
-processaTexto(caminho);
+processaTexto(caminho); 
 
 // escrever node cli.js ./arquivos/texto1.md no console para visualizar
 
