80599182510a3428d07d230096fc1997a164ef63
Danilo Araujo <dnlysid@gmail.com>
2022-01-10 17:40:32 -0300

    validando links

--- lib-markdown/cli.js
+++ lib-markdown/cli.js
@@ -1,10 +1,16 @@
-import pegaArquivo from './regex';
+import chalk from 'chalk';
+import pegaArquivo from './index';
+import validaURLs from './http-validacao';
 
 const caminho = process.argv;
 
 async function processaTexto(caminhoDoArquivo) {
     const resultado = await pegaArquivo(caminhoDoArquivo[2]);
-    console.log('Lista de Links', resultado)
+    if (caminho[3] === 'validar') {
+        console.log(chalk.yellow('Links Validados'), validaURLs(resultado));
+    } else {
+        console.log(chalk.yellow('Lista de Links'), resultado)
+    }
 }
 
 processaTexto(caminho);
---
+++ lib-markdown/http-validacao.js
@@ -0,0 +1,5 @@
+function validaURLs(arrayLinks) {
+    return validaURLs;
+}
+
+module.exports = validaURLs;
