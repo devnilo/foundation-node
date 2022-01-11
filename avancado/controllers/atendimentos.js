f1b57f8407bc36e0e906cf168882d62fbb873279
Danilo Araujo <dnlysid@gmail.com>
2022-01-11 20:50:59 -0300

    API com Express e MySQL

---
+++ avancado/controllers/atendimentos.js
@@ -0,0 +1,10 @@
+const res = require("express/lib/response")
+
+module.exports = app => {
+    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos!'))
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Você está na rota de atendimentos, e está realizando um post!')
+    })
+}
