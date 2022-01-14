28300cbd3e792b46b607158510db2e3f4921831c
Danilo Araujo <dnlysid@gmail.com>
2022-01-14 16:20:16 -0300

    finalizando API

--- avancado/controllers/atendimentos.js
+++ avancado/controllers/atendimentos.js
@@ -2,13 +2,34 @@ const res = require("express/lib/response")
 const Atendimento = require('../models/atendimentos');
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos!'))
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req,res) => {
+        const id = parseInt(req.params.id)
+        
+        Atendimento.buscaPorId(id, res)
+    })
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body;
 
         Atendimento.adiciona(atendimento)
         res.send('Você está na rota de atendimentos, e está realizando um post!')
+    })
+
+    app.patch('./atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+
+        Atendimento.altera(id, valores, res)
+    })
+
+    app.delete('./atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.deleta(id, res)
     })
 }
 
