e2752efa03ebaed4133536a01b5324150b05a197
Danilo Araujo <dnlysid@gmail.com>
2022-03-07 17:53:14 -0300

    trabalhando com EventEmitter

---
+++ nodeBR/02-events/index.js
@@ -0,0 +1,38 @@
+const EventEmitter = require('events')
+class MeuEmissor extends EventEmitter {
+
+}
+
+const meuEmissor = new MeuEmissor()
+const nomeEvento = 'usuario:click'
+
+meuEmissor.on(nomeEvento, function (click) {
+    console.log('um usuario clicou', click)
+})
+
+const stdin = process.openStdin()
+
+function main() {
+    return new Promise(function(resolve, reject) {
+        stdin.addListener('data', function (value) {
+    //      console.log(`Você digitou: ${value.toString().trim()}`)
+            return resolve(value)
+        })
+    })
+}
+
+main().then(function(resultado) {
+    console.log('resultado', resultado.toString())
+})
+
+/* a ideia do promise é executar só uma vez, e os eventos pra manipulação contínua */
+
+/* 
+meuEmissor.emit(nomeEvento, 'na barra de rolagem')
+meuEmissor.emit(nomeEvento, 'no ok')
+
+let count = 0
+setInterval(function() {
+    meuEmissor.emit(nomeEvento, 'no ok' + (count++))
+}, 1000) 
+*/
