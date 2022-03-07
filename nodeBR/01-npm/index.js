b98a0f8b8db8b935af9d4d3bfb85ffb6c8685e20
Danilo Araujo <dnlysid@gmail.com>
2022-03-07 16:51:10 -0300

    refatorando callbacks para promises

--- nodeBR/01-npm/index.js
+++ nodeBR/01-npm/index.js
@@ -4,52 +4,100 @@
 - obter o endereço do usuario pelo ID
 */
 
-function obterUsuario(callback) {
-    setTimeout(function () {
-        return callback(null, {
-            id: 1,
-            nome: 'Juca',
-            dataNascimento: new Date() 
-        }
-    )}, 1000);
+/* importamos um módulo interno do node.js para deixar o obterEndereco assincrono sem precisar 
+escrever o código que escrevemos no obterUsuario e obterTelefone. PS: alguns momentos do dia dia, não
+conseguirei utilizar o promisify(util), pq algumas libs não seguem o padrão de erro e resposta de
+callbacks, ai sim teremos que converter para new Promise */
+const util = require('util')
+const obterEnderecoAsync = util.promisify(obterEndereco)
+
+function obterUsuario() {
+    // quando der algum problema -> reject(ERRO)
+    // quando sucess -> resolv
+    return new Promise(function resolvePromise(resolv, reject) {
+        setTimeout(function () {
+            return resolv({
+                id: 1,
+                nome: 'Juca',
+                dataNascimento: '07/07/2007'
+            })
+        }, 1000)
+    })
 }
 
-function obterTelefone(idUsuario, callback) {
-    setTimeout(function () {
-        return callback(null, {
-            telefone: '97777-7777',
-            ddd: 11
-        }
-    )}, 2000);
-
+function obterTelefone(idUsuario) {
+    return new Promise(function resolverPromise(resolv, reject) {
+        setTimeout( () => {
+            return resolv({
+                telefone: '97777-7777',
+                ddd: 11
+            })
+        }, 2000)
+    })
 }
 
 function obterEndereco(idUsuario, callback) {
     setTimeout( () => {
         return callback(null, {
-            rua: 'da sorte',
+            rua: 'Rua da Sorte',
             numero: 7
         })
     }, 2000);
 }
 
-function resolverUsuario(erro, usuario) {
-    console.log('usuario', usuario)
-}
+const usuarioPromise = obterUsuario()
+// para manipular o sucesso usamos a função .then
+// para manipular erros usamos .catch
 
-obterUsuario(function resolverUsuario(error, usuario) {
-    if(error) {
+usuarioPromise
+    .then(function(usuario) {
+        return obterTelefone(usuario.id)
+        .then(function resolverTelefone(result) {
+            return {
+                usuario: {
+                    nome: usuario.nome,
+                    id: usuario.id
+                },
+
+                telefone: result
+            }
+        })
+    })
+    .then(function(resultado) {
+        const endereco = obterEnderecoAsync(resultado.usuario.id)
+        return endereco.then(function resolverEndereco(result) {
+            return {
+                usuario: resultado.usuario,
+                telefone: resultado.telefone,
+                endereco: result
+            }
+        })
+    })
+    .then(function(resultado) {
+        console.log(`
+            Nome: ${resultado.usuario.nome}
+            Telefone: (${resultado.telefone.ddd}) ${resultado.telefone.telefone}
+            Endereço: ${resultado.endereco.rua}, ${resultado.endereco.numero}
+        `)
+    })
+    .catch(function(error) {
+        console.error('DEU RUIM PAI!', error)
+    })
+
+
+/* obterUsuario(function resolverUsuario(error, usuario) {
+    if (error) {
         console.error('DEU RUIM em USUARIO', error)
         return;
     }
 
     obterTelefone(usuario.id, function resolverTelefone(error1, telefone) {
-        if(error1) {
+        if (error1) {
             console.error('DEU RUIM em TELEFONE', error1)
             return;
         }
         obterEndereco(usuario.id, function resolverEndereco(error2, endereco) {
-            if(error2) {
+            if (error2) {
                 console.error('DEU RUIM em ENDEREÇO', error2)
                 return;
             }
@@ -61,7 +109,7 @@ obterUsuario(function resolverUsuario(error, usuario) {
             `)
         })
     })
-});
+}); */
 
 // const telefone = obterTelefone(usuario.id);
 
